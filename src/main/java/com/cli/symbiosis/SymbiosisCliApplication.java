419e749b867da5dd4160c8784f7ac88f26369c8f
Lakiduck <lwalgama@hotmail.com>
2022-09-03 11:54:06 +1000

    basic get and put data commands implemented

--- src/main/java/com/cli/symbiosis/SymbiosisCliApplication.java
+++ src/main/java/com/cli/symbiosis/SymbiosisCliApplication.java
@@ -1,7 +1,13 @@
 package com.cli.symbiosis;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class SymbiosisCliApplication {
@@ -11,3 +17,39 @@ public class SymbiosisCliApplication {
 	}
 
 }
+
+@Service
+class DataService {
+;
+	private ArrayList<String[]> data = new ArrayList<>();
+
+	void addItem(String material, String quantity) {
+		final String[] dataRecord = {material,quantity};
+		this.data.add(dataRecord);
+	}
+
+	ArrayList<String[]> getAllItems() {
+		return data;
+	}
+}
+
+
+@ShellComponent
+record SymbiosisCommands ( DataService service ) {
+
+	@ShellMethod ("put-data")
+	public void putData (String material, String quantity) {
+		this.service.addItem(material, quantity);
+	}
+
+	@ShellMethod ("get-data")
+	public void getData () {
+		ArrayList<String[]> items = this.service.getAllItems();
+		for(String[] i : items){
+			System.out.println(Arrays.toString(i));
+		}
+	}
+
+
+}
+
